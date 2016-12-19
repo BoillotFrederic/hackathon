cd5084c0f181c353221fba6bb229592db5f8d7f9
040975 <vidalchri62@gmail.com>
2016-12-19 16:36:10 +0100

    fichier

--- inc/winner.php
+++ inc/winner.php
@@ -9,12 +9,12 @@
       <h3 class="blink"> Gagnant de la semaine!</h3>
      </div>
      <div   class="row">
-     <div  id="photo" class="col l8 m8 s8 left-align">
+     <div class="col l8 m8 s8 left-align">
 
        <img id="gagnant" src="imgs/zelda.png" alt="zelda">
 
      </div>
-    <div  id="commentaire"class="col l4 m4 s4 center-align">
+    <div id="commentaire"class="col l4 m4 s4 center-align">
       <p> Commentaires :</p>
         <div id="com1">pppp</div>
         <div id="com2">pppp</div>
