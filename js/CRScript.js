3903f13455ac37dc28fa674f2f7b3d2cd31d50f8
040975 <vidalchri62@gmail.com>
2016-12-19 16:14:53 +0100

    fichier

--- js/CRScript.js
+++ js/CRScript.js
@@ -2,4 +2,11 @@
 //logon parallax
 $(document).ready(function(){
       $('.parallax').parallax();
+      $(window).resize(function(){onReSize();});
+      onReSize();
     });
+
+function onReSize()
+{
+  $('#commentaire').height($('#gagnant').height());
+}
