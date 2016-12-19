90fb478ba14638956e1ab39457a098c399d67f80
BoillotFrederic <boillot.frederic.62@gmail.com>
2016-12-19 13:22:15 +0100

    CHTISELFIES

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     <title>Ch'tis Selfies</title>
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/FBStyle.css">
+    <link rel="stylesheet" href="css/CRStyle.css">
+    <link rel="stylesheet" href="css/FRStyle.css">
+    <link rel="stylesheet" href="css/ELStyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
   <body>
-    <<?php
+    <?php
 
       include 'inc/navBar.php';
       include 'inc/logo.php';
@@ -17,8 +20,11 @@
       include 'inc/carousel.php';
 
     ?>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
-    <script src="js/jScript.js"></script>
+    <script src="js/FBScript.js"></script>
+    <script src="js/CRScript.js"></script>
+    <script src="js/FRScript.js"></script>
+    <script src="js/ELScript.js"></script>
   </body>
 </html>
