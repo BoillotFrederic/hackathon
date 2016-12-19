0862dca1d677268d6d407a335a01b5d508858b38
BoillotFrederic <boillot.frederic.62@gmail.com>
2016-12-19 13:37:21 +0100

    CHTI

--- index.php
+++ index.php
@@ -12,14 +12,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
   <body>
+    <div class="row">
+      <div class="col s3 m3 l3">
+        <?php include 'inc/navBar.php'; ?>
+      </div>
+      <div class="col s9 m9 l9">
     <?php
 
-      include 'inc/navBar.php';
       include 'inc/logo.php';
       include 'inc/description.php';
       include 'inc/carousel.php';
 
     ?>
+      </div>
+    </div>
     <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
     <script src="js/FBScript.js"></script>
