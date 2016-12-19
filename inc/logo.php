5f002811b0d0236e8ad2833dd3f85f41016a6c18
040975 <vidalchri62@gmail.com>
2016-12-19 13:55:07 +0100

    chris

---
+++ inc/logo.php
@@ -0,0 +1,9 @@
+
+
+    <div class="parallax-container">
+      <div class="row">
+        <div class="col l12 m12 s12">
+      <div class="parallax"><img src="imgs/boulognesur.jpg"alt="boulognesurmer"></div>
+    </div>
+  </div>
+</div>
