941b2f6501cff69763756e382cf7016c943fbf46
edaudenthun <edaudenthun@gmail.com>
2016-12-19 14:13:30 +0100

    push elise description

---
+++ inc/description.php
@@ -0,0 +1,4 @@
+<div id="description" class="col s12 m12 l12">
+  <h1> A vos "Chelfies" ! </h1>
+  <p> N'as tu jamais rêvé de te retrouver en première page d'un site internet ? Et bien nous pouvons réaliser ton rêve ! Les règles sont simples : Prends la meilleure photo de toi devant un monument Boulonnais, puis poste là ! La photo qui remporte le plus de votes sera publiée en première page de notre site ! Vous êtes prêt ? 3, 2, 1 ... Selfie ! </p>
+</div>
