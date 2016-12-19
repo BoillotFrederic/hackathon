9cc72d072adf255e9fc5c54c8d8c48fe131f27cd
040975 <vidalchri62@gmail.com>
2016-12-19 20:37:28 +0100

    fichier

--- inc/winner.php
+++ inc/winner.php
@@ -6,7 +6,9 @@
 <div class="container">
    <div class="row">
      <div class="col l12 m12 s12 center-align">
+          <p>Attention il reste plus que </p><p id="Compte"></p><p>pour participer</p>
       <h3 class="blink"> Gagnant de la semaine!</h3>
+
      </div>
      <div   class="row">
      <div class="col l8 m8 s12 ">
