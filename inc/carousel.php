a03385ff37839066ccdd98072fb38eee3017b321
Francois62 <franois.andrzejewski@laposte.net>
2016-12-19 16:13:25 +0100

    test

--- inc/carousel.php
+++ inc/carousel.php
@@ -1,15 +1,15 @@
 <div class="row">
   <div class="col m12 s12 l12">
-<div id="car"class="carousel center-align">
-  <a class="carousel-item active" href="#one!"><h1>2nd</h1><img src="imgs/zelda.png" class="carim center-block" >coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
-  <a class="carousel-item active" href="#two!"><h1>3rd</h1><img src="imgs/zelda.png" class="carim center-block">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
-  <a class="carousel-item active" href="#three!"><h1>4rth</h1><img src="imgs/zelda.png" class="carim center-block">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
-  <a class="carousel-item active" href="#four!"><h1>5th</h1><img src="imgs/zelda.png" class="carim center-block">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
-  <a class="carousel-item active" href="#five!"><h1>6th</h1><img src="imgs/zelda.png" class="carim center-block">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
-  <a class="carousel-item active" href="#five!"><h1>7th</h1><img src="imgs/zelda.png" class="carim center-block">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
-  <a class="carousel-item active" href="#five!"><h1>8th</h1><img src="imgs/zelda.png" class="carim center-block">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
-  <a class="carousel-item active" href="#five!"><h1>9th</h1><img src="imgs/zelda.png" class="carim center-block">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
-  <a class="carousel-item active" href="#five!"><h1>10th</h1><img src="imgs/zelda.png" class="carim center-block">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</a>
+<div class="carousel center-align">
+  <a class="carousel-item active" href="#one!"><h1 class="titrecar">2nd</h1><img src="imgs/zelda.png" class="carim center-block" ><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
+  <a class="carousel-item active" href="#two!"><h1 class="titrecar">3rd</h1><img src="imgs/zelda.png" class="carim center-block"><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
+  <a class="carousel-item active" href="#three!"><h1 class="titrecar">4th</h1><img src="imgs/zelda.png" class="carim center-block"><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
+  <a class="carousel-item active" href="#four!"><h1 class="titrecar">5th</h1><img src="imgs/zelda.png" class="carim center-block"><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
+  <a class="carousel-item active" href="#five!"><h1 class="titrecar">6th</h1><img src="imgs/zelda.png" class="carim center-block"><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
+  <a class="carousel-item active" href="#five!"><h1 class="titrecar">7th</h1><img src="imgs/zelda.png" class="carim center-block"><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
+  <a class="carousel-item active" href="#five!"><h1 class="titrecar">8th</h1><img src="imgs/zelda.png" class="carim center-block"><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
+  <a class="carousel-item active" href="#five!"><h1 class="titrecar">9th</h1><img src="imgs/zelda.png" class="carim center-block"><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
+  <a class="carousel-item active" href="#five!"><h1 class="titrecar">10th</h1><img src="imgs/zelda.png" class="carim center-block"><p class="elise">coolos58 qui a pris sa photo devant la cathedral de Boulogne sur mer</p></a>
 </div>
 </div>
 </div>
