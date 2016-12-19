5559a448018c7b9c16688da8e5d330ac6bd034f9
BoillotFrederic <boillot.frederic.62@gmail.com>
2016-12-19 12:09:26 +0100

    ChtisSelfies

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-Test
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ch'tis Selfies</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+  <body>
+    <<?php
+
+      include 'inc/navBar.php';
+      include 'inc/logo.php';
+      include 'inc/description.php';
+      include 'inc/carousel.php';
+
+    ?>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script src="js/jScript.js"></script>
+  </body>
+</html>
