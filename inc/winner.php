6059266d8511c1343743cddd8c62254339099f03
040975 <vidalchri62@gmail.com>
2016-12-20 13:33:10 +0100

    fichier

--- inc/winner.php
+++ inc/winner.php
@@ -12,12 +12,28 @@
 
      </div>
      <div   class="row">
-     <div class="col l8 m8 s12 ">
+     <div class="col l12 m12 s12 left-align">
+        <img id="gagnant" onclick="clickimage();"src="imgs/zelda.png" alt="zelda">
+   </div>
+   <div   class="row">
+   <div class="col l12 m12 s12 left-align">
+     <!-- Modal Trigger -->
+   <a class="waves-effect waves-light btn" href="#modal1">Modal</a>
 
-       <img id="gagnant" onclick="clickimage();"src="imgs/zelda.png" alt="zelda">
-
+   <!-- Modal Structure -->
+   <div id="modal1" class="modal bottom-sheet">
+     <div class="modal-content">
+       <h4>Modal Header</h4>
+       <p>A bunch of text</p>
      </div>
-    <div id="commentaire"class="col l4 m4 s12 center-align">
+     <div class="modal-footer">
+       <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+     </div>
+   </div>
+           
+   </div>
+ </div>
+    <!--<div id="commentaire"class="col l4 m4 s12 left-align">
       <p>Commentaires :</p>
         <div id="com1">pppp</div>
         <div id="com2">pppp</div>
@@ -33,7 +49,7 @@
         <div id="com4">pppp</div>
         <div id="com3">ppppp</div>
         <div id="com4">ppppp</div>
-  </div>
+  </div>-->
  </div>
 </div>
 </div>
