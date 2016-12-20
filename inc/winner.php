f5a640e9d362b48e76bac1434e39e8b90046f628
040975 <vidalchri62@gmail.com>
2016-12-20 13:40:48 +0100

    fichier

--- inc/winner.php
+++ inc/winner.php
@@ -5,18 +5,18 @@
 
 <div class="container">
    <div class="row">
-     <div class="col l12 m12 s12 center-align">
+     <div class="col l12 m12 s12 center-align valign-wrapper">
           <p id="Compte"></p>
-
+</div>
+ <div class="col l12 m12 s12 center-align">
       <h3 class="blink"> Gagnant de la semaine!</h3>
-
+</div>
      </div>
      <div   class="row">
      <div class="col l12 m12 s12 left-align">
         <img id="gagnant" onclick="clickimage();"src="imgs/zelda.png" alt="zelda">
    </div>
-   <div   class="row">
-   <div class="col l12 m12 s12 left-align">
+
      <!-- Modal Trigger -->
    <a class="waves-effect waves-light btn" href="#modal1">Modal</a>
 
@@ -30,9 +30,8 @@
        <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
      </div>
    </div>
-           
-   </div>
- </div>
+
+
     <!--<div id="commentaire"class="col l4 m4 s12 left-align">
       <p>Commentaires :</p>
         <div id="com1">pppp</div>
