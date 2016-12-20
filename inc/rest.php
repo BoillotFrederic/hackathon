89bb99e96d006e70a1e4e4679cbad217649216d4
edaudenthun <edaudenthun@gmail.com>
2016-12-20 09:06:35 +0100

    centrage des photos elise

--- inc/rest.php
+++ inc/rest.php
@@ -1,47 +1,46 @@
 
-<div id="rest" class="col l12 m12 s12 center">
+<div id="rest" class="col l12 m12 s12 center ">
   <h4> Le reste des participants : </h4>
 
-  <div class="row">
+  <div class="row valign-wrapper">
 
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
+    <div class="col l4 s4 m4 rest">
+      <img class="responsive-img" src="imgs/selfie1.jpg">
     </div>
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
+    <div class="col l4 s4 m4 rest  ">
+      <img   class="responsive-img"src="imgs/selfie2.jpg">
     </div>
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
-    </div>
-
-  </div>
-
-  <div class="row">
-
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
-    </div>
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
-    </div>
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
+    <div class="col l4 s4 m4 rest  ">
+      <img   class="responsive-img"src="imgs/zelda.png">
     </div>
 
   </div>
 
   <div class="row">
 
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
+    <div class="col l4 s4 m4 rest  ">
+      <img   class="responsive-img"src="imgs/zelda.png">
     </div>
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
+    <div class="col l4 s4 m4 rest  ">
+      <img  class="responsive-img" src="imgs/zelda.png">
     </div>
-    <div class="col l4 s4 m4">
-      <img src="imgs/zelda.png">
+    <div class="col l4 s4 m4 rest  ">
+      <img   class="responsive-img"src="imgs/zelda.png">
     </div>
 
   </div>
 
+  <div class="row">
+
+    <div class="col l4 s4 m4 rest  ">
+      <img  class="responsive-img" src="imgs/zelda.png">
+    </div>
+    <div class="col l4 s4 m4 rest  ">
+      <img class="responsive-img"  src="imgs/zelda.png">
+    </div>
+    <div class="col l4 s4 m4 rest  ">
+      <img class="responsive-img"  src="imgs/zelda.png">
+    </div>
+
+  </div>
 </div>
