8eda2afc5e30d2be103c8d364ccee7502fdab97f
Francois62 <franois.andrzejewski@laposte.net>
2016-12-20 09:25:34 +0100

    test

--- js/FRScript.js
+++ js/FRScript.js
@@ -3,4 +3,13 @@
 // $('.carousel').carousel('next');
 // $('.carousel').carousel('next', 9); // Move next n times.
 // $('.carousel').carousel('set', 4);
+$('.carousel').carousel({
+    padding: 200
+});
+autoplay()
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4000);
+}
+
 $('.carousel.carousel-slider').carousel({full_width: true});
