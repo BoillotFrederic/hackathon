c0bd9a0dd18e42866411f2f0fc3a1bf8b74d343d
040975 <vidalchri62@gmail.com>
2016-12-20 14:52:58 +0100

    fichier

--- inc/winner.php
+++ inc/winner.php
@@ -7,12 +7,13 @@
    <div class="row">
      <div class="col l12 m12 s12 center-align">
           <p id="Compte"></p>
-</div>
- <div class="col l12 m12 s12 center-align">
+    </div>
+   <div class="col l12 m12 s12 center-align">
       <h3 class="blink"> Gagnant de la semaine!</h3>
-</div>
+  </div>
      </div>
-     <div   class="row">
+
+     <div  class="row">
      <div class="col l12 m12 s12 left-align">
          <img id="gagnant" onclick="clickimage();"src="imgs/zelda.png" alt="zelda">
        <a id="boutonmodal" class="waves-effect waves-light btn" href="#modal1">Commentaires</a>
@@ -21,16 +22,19 @@
 
      <!-- Modal Trigger -->
 
-
    <!-- Modal Structure -->
    <div id="modal1" class="modal bottom-sheet">
      <div class="modal-content">
        <h4>COMMENTAIRES</h4>
-       <p>A bunch of text</p>
+      
        <div class="com" id="comm">ppp</div>
        <div class="com" id="comm1">ppp</div>
        <div class="com" id="comm2">ppp</div>
        <div class="com" id="comm3">ppp</div>
+       <div class="com" id="comm4">ppp</div>
+       <div class="com" id="comm5">ppp</div>
+       <div class="com" id="comm6">ppp</div>
+       <div class="com" id="comm7">ppp</div>
      </div>
      <div class="modal-footer">
        <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">FERMER</a>
@@ -38,23 +42,6 @@
    </div>
 
 
-    <!--<div id="commentaire"class="col l4 m4 s12 left-align">
-      <p>Commentaires :</p>
-        <div id="com1">pppp</div>
-        <div id="com2">pppp</div>
-        <div id="com3">pppp</div>
-        <div id="com4">pppp</div>
-        <div id="com3">ppppp</div>
-        <div id="com4">ppppp</div>
-        <div id="com4">pppp</div>
-        <div id="com3">ppppp</div>
-        <div id="com4">ppppp</div>
-        <div id="com3">ppppp</div>
-        <div id="com4">ppppp</div>
-        <div id="com4">pppp</div>
-        <div id="com3">ppppp</div>
-        <div id="com4">ppppp</div>
-  </div>-->
+
  </div>
-</div>
 </div>
