b8e973f92405ecb4c094d63a585b83b5f0e8dd49
edaudenthun <edaudenthun@gmail.com>
2016-12-20 14:40:32 +0100

    modif bug elise

--- inc/rest.php
+++ inc/rest.php
@@ -5,55 +5,38 @@
 
     <div class="col l4 s12 m6 rest">
       <img class="responsive-img" src="imgs/selfie1.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
     </div>
+
     <div class="col l4 s12 m6 rest">
       <img   class="responsive-img" src="imgs/selfie2.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
     </div>
-    <div class="col l4 s12 m6 rest ">
+
+    <div class="col l4 s12 m6 rest">
       <img class="responsive-img" src="imgs/selfie3.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
     </div>
 
-
-    <div class="col l4 s12 m6 rest ">
+    <div class="col l4 s12 m6 rest">
       <img class="responsive-img" src="imgs/selfie4.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
     </div>
-    <div class="col l4 s12 m6 rest  ">
+
+    <div class="col l4 s12 m6 rest">
       <img class="responsive-img" src="imgs/selfie5.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
     </div>
-    <div class="col l4 s12 m6 rest  ">
+
+    <div class="col l4 s12 m6 rest">
       <img class="responsive-img" src="imgs/selfie6.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
     </div>
 
-
-
-    <div class="col l4 s12 m6 rest ">
+    <div class="col l4 s12 m6 rest">
       <img class="responsive-img" src="imgs/selfie7.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
     </div>
-    <div class="col l4 s12 m6 rest  ">
+
+    <div class="col l4 s12 m6 rest">
       <img class="responsive-img"  src="imgs/selfie8.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
-
     </div>
 
-    <div class="col l4 s12 m6 rest  ">
+    <div class="col l4 s12 m6 rest">
       <img class="responsive-img"  src="imgs/selfie9.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-
-
     </div>
 
 
