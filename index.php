d2f4a7aab74e9348819a29d6b0b3c4e85dd87f0d
BoillotFrederic <boillot.frederic.62@gmail.com>
2016-12-21 10:43:20 +0100

    Frédo

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
   <body>
+    <?php include 'inc/goToTop.php'; ?>
     <div class="valign-wrapper teal darken-4" id="navBar">
       <?php include 'inc/navBar.php'; ?>
     </div>
