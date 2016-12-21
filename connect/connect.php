29737d19a5a2072f84e3fd1cbfa29fc2f1d1add2
BoillotFrederic <boillot.frederic.62@gmail.com>
2016-12-21 16:56:46 +0100

    Frédo

--- connect/connect.php
+++ connect/connect.php
@@ -5,7 +5,11 @@ try
     // 192.168.1.37 // FREDO
     $db = new PDO("mysql:dbname=hackathon;host=192.168.1.37", "root2", "Az3rty", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     // François
-    //$db = new PDO("mysql:dbname=hackathon;host=localhost", "root2", "CodeOp62", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    //$db = new PDO("mysql:dbname=hackathon;host=localhost", "root", "CodeOp62", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    // Elise
+    //$db = new PDO("mysql:dbname=hackathon;host=localhost", "root", "pokemon1910", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    // Chris
+    //$db = new PDO("mysql:dbname=hackathon;host=localhost", "root", "vidal62", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 }
 catch (PDOException $e)
 {
