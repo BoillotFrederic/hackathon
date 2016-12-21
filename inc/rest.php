a8380a3d3b23f7b5ea441c1a9c199b3d32506f51
edaudenthun <edaudenthun@gmail.com>
2016-12-21 10:12:42 +0100

    push de elise pour fred

--- inc/rest.php
+++ inc/rest.php
@@ -1,43 +1,22 @@
-
 <div id="rest" class="row center ">
   <h4 class="col s12 m12 l12 "> Les participants : </h4>
 
+<?php
 
-    <div class="col l4 s12 m6 rest">
-      <img class="responsive-img" src="imgs/selfie1.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-    <div class="col l4 s12 m6 rest">
-      <img   class="responsive-img" src="imgs/selfie2.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-    <div class="col l4 s12 m6 rest">
-      <img class="responsive-img" src="imgs/selfie3.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-    <div class="col l4 s12 m6 rest">
-      <img class="responsive-img" src="imgs/selfie4.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-    <div class="col l4 s12 m6 rest">
-      <img class="responsive-img" src="imgs/selfie5.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-    <div class="col l4 s12 m6 rest">
-      <img class="responsive-img" src="imgs/selfie6.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-    <div class="col l4 s12 m6 rest">
-      <img class="responsive-img" src="imgs/selfie7.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-    <div class="col l4 s12 m6 rest">
-      <img class="responsive-img"  src="imgs/selfie8.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-    <div class="col l4 s12 m6 rest">
-      <img class="responsive-img"  src="imgs/selfie9.jpg"><i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i><i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
-    </div>
-
-
+$query = $db->prepare('SELECT * from participant  WHERE adddate > "'.date("Y/m/d", $lastMonday).'"  ORDER BY adddate DESC LIMIT 0,9');
+$query->execute();
+while($restData = $query->fetch())
+{
+?>
+  <div class="col l4 s12 m6 rest">
+    <div class="restpseudo"><?php echo $restData['pseudo']; ?></div>
+    <img class="responsive-img" src="uploads/<?php echo $restData['img']; ?>">
+    <div class="nbrlike"><?php echo $restData['islike']; ?></div>
+    <i class="like fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i>
+    <div class="nbrdislike"><?php echo $restData['dontlike']; ?></div>
+    <i class="dislike fa fa-thumbs-o-down fa-3x" aria-hidden="true"></i>
+  </div>
+<?php
+  }
+?>
 </div>
