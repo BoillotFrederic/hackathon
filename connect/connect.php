14f0c885647afa4fb38681a1b931c7e97e6c0739
Francois62 <franois.andrzejewski@laposte.net>
2016-12-21 21:00:07 +0100

    test

--- connect/connect.php
+++ connect/connect.php
@@ -3,9 +3,9 @@
 try
 {
     // 192.168.1.37 // FREDO
-    $db = new PDO("mysql:dbname=hackathon;host=192.168.1.37", "root2", "Az3rty", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    //$db = new PDO("mysql:dbname=hackathon;host=192.168.1.37", "root2", "Az3rty", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     // François
-    //$db = new PDO("mysql:dbname=hackathon;host=localhost", "root", "CodeOp62", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    $db = new PDO("mysql:dbname=hackathon;host=localhost", "root", "CodeOp62", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     // Elise
     //$db = new PDO("mysql:dbname=hackathon;host=localhost", "root", "pokemon1910", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     // Chris
